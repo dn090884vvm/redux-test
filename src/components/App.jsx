6fe2b7d1a6541b6764910a9f5f6030c48516f240
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-08-21 15:40:09 +0300

    xxx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,23 @@
+// export const App = () => {
+//   return <div>hihihi</div>;
 
-export const App = () => {
-  return <div>hihihi</div>;
+// import { useSelector, useDispatch } from 'react-redux';
+// import { increment, decrement } from 'redux/store';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from 'redux/store';
+// export const App = () => {
+//   const dispatch = useDispatch();
+//   const value = useSelector(state => state.myValue);
+//   console.log(value);
+//   return (
+//     <div>
+//       {value}
+//       <button type="button" onClick={() => dispatch(increment(1))}>
+//         Increment
+//       </button>
+//       <button type="button" onClick={() => dispatch(decrement(1))}>
+//         Decrement
+//       </button>
+//     </div>
+//   );
 
-export const App = () => {
-  const dispatch = useDispatch();
-  const value = useSelector(state => state.myValue);
-  console.log(value);
-  return (
-    <div>
-      {value}
-      <button type="button" onClick={() => dispatch(increment(1))}>
-        Increment
-      </button>
-      <button type="button" onClick={() => dispatch(decrement(1))}>
-        Decrement
-      </button>
-    </div>
-  );
-
-};
+// };
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import { namesArrow } from './timerReducer';
 
 const store = configureStore({
@@ -9,29 +8,3 @@ const store = configureStore({
 });
 
 export default store;
-
-import { createAction, createReducer } from '@reduxjs/toolkit';
-
-export const increment = createAction('myValue/increment');
-export const decrement = createAction('myValue/decrement');
-
-const myReducer = createReducer(0, {
-  [increment]: (state, action) => state + action.payload,
-  [decrement]: (state, action) => state - action.payload,
-});
-
-export const add = createAction('items/add');
-export const remove = createAction('items/remove');
-
-const itemsReducer = createReducer([], {
-  [add]: (state, action) => [...state, action.payload],
-  // [add]: (state, action) => state.push(action.payload),
-  [remove]: (state, action) => state.filter(item => item.id !== action.payload),
-});
-
-export const store = configureStore({
-  reducer: {
-    myValue: myReducer,
-    items: itemsReducer,
-  },
-});
